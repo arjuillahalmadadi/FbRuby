76055263c1c6a7a5ae48b9434dc4a99ed4fb089c
Rahmat adha <68039885+MR-X-junior@users.noreply.github.com>
2024-07-08 15:41:37 +0800

    Add files via upload

---
+++ lib/FbRuby.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+#--
+# Copyright 2024 by Rahmat adha
+# All rights reserved.
+# See LICENSE for permissions.
+#++
+
+require_relative "fbruby/user.rb"
+require_relative "fbruby/posts.rb"
+require_relative "fbruby/login.rb"
+require_relative "fbruby/chats.rb"
+require_relative "fbruby/utils.rb"
+require_relative "fbruby/groups.rb"
+require_relative "fbruby/tempmail.rb"
+require_relative "fbruby/settings.rb"
+require_relative "fbruby/facebook.rb"
+require_relative "fbruby/comments.rb"
+require_relative "fbruby/messenger.rb"
+require_relative "fbruby/exceptions.rb"
+require_relative "fbruby/createaccount.rb"
